11aa21623d6d3a3f38175c33ba092f843909557d
SanjanaK1906 <sanjanakumbhar199@gmail.com>
2025-02-07 15:44:37 +0530

    error_resolve

--- vidyasetu/backend/src/main/java/com/vidyasetu/model/Student.java
+++ vidyasetu/backend/src/main/java/com/vidyasetu/model/Student.java
@@ -1,11 +1,13 @@
 package com.vidyasetu.model;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+// import javax.persistence.ManyToOne;
+// import javax.persistence.JoinColumn;
+import jakarta.persistence.*;
+
 
 @Entity
 @Table(name = "students")
