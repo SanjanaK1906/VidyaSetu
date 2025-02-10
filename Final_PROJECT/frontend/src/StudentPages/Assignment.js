98affd12ad94281cdd3fc8249a8bcfeb9182c1cd
Rishabh Singh <rishabh000888@gmail.com>
2025-02-10 10:58:35 +0530

    Front end ui changes in upload assignment and student page

--- Final_PROJECT/frontend/src/StudentPages/Assignment.js
+++ Final_PROJECT/frontend/src/StudentPages/Assignment.js
@@ -132,7 +132,7 @@ function Assignment() {
                   <th>Upload</th>
                 </tr>
               </thead>
-              <tbody>
+              {/* <tbody>
                 {
                   data.assignments.filter((val) => {
                     if (searchText == "") {
@@ -150,15 +150,55 @@ function Assignment() {
                         <td><button className="btn btn-primary" onClick={() => handleDownload(fileName)}> <i class="bi bi-box-arrow-in-down"></i> Download</button></td>
                         <div className="field">
                           <label>Upload File</label><br></br>
-                          <input type="file"
-                            name="file" onChange={(e) => handleFile(e, id)}
-                          /></div>
-                        <button className="btn btn-info" onClick={(e) => submitForm(e)}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+                          <input type="file"name="file" onChange={(e) => handleFile(e, id)} /></div>
+                          <button className="btn btn-info" onClick={(e) => submitForm(e)}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
   <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
   <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
 </svg> Upload</button>
                       </tr>)}
-              </tbody>
+              </tbody> */}
+              <tbody>
+  {
+    data.assignments.filter((val) => {
+      if (searchText === "") {
+        return val;
+      } else if (val.moduleName.toLowerCase().includes(searchText.toLowerCase()) || val.facultyName.toLowerCase().includes(searchText.toLowerCase())) {
+        return val;
+      }
+    })
+    .map(({ id, facultyName, moduleName, description, fileName }) =>
+      <tr key={id}>
+        <td>{id}</td>
+        <td>{facultyName}</td>
+        <td>{moduleName}</td>
+        <td>{description}</td>
+        <td>
+          <button className="btn btn-primary" onClick={() => handleDownload(fileName)}> 
+            <i className="bi bi-box-arrow-in-down"></i> Download
+          </button>
+        </td>
+        <td>
+          {/* Flex container to align the file input and upload button in a row */}
+          <div className="d-flex align-items-center">
+            <div className="me-2"> {/* Add margin to the right of the input */}
+              <input 
+                type="file" 
+                name="file" 
+                onChange={(e) => handleFile(e, id)} 
+              />
+            </div>
+            <button className="btn btn-info" onClick={(e) => submitForm(e)}> 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-upload" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
+              </svg> 
+              Upload
+            </button>
+          </div>
+        </td>
+      </tr>
+    )}
+</tbody>
             </table>
           </div>
         </div>
